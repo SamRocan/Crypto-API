887ec851680384ee6e3239bbd090698da8710d04
SamRocan <SamClendenan@iCloud.com>
2022-01-23 12:01:41 +0000

    Dark mode initialized

--- cryptoApi/static/js/currency_page_script.js
+++ cryptoApi/static/js/currency_page_script.js
@@ -1,1 +1,22 @@
 console.log("Working");
+
+function dark_light_mode(){
+    var checkBox = document.getElementById("dark_light");
+
+    if (checkBox.checked == true){
+        console.log("Dark");
+        document.body.style.background = '#040c1f';
+        document.body.style.backgroundImage = 'linear-gradient(315deg, #3a496e 0%, #040c1f 74%)'
+        containers = document.getElementsByClassName('currency-info-section')
+        for (i = 0; i < containers.length; i++) {
+            containers[i].style.color = 'rgba(233,233,233,0.8)';
+        }
+
+    } else {
+        document.body.style.backgroundColor = '#f8f9d2';
+        document.body.style.backgroundImage = 'linear-gradient(315deg, #f8f9d2 0%, #e8dbfc 74%)'
+        for (i = 0; i < containers.length; i++) {
+            containers[i].style.color = 'rgba(14, 50, 92, 0.6)';
+        }
+    }
+}
